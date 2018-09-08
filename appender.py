20936f5febd110cd4da1a2f02ead27213d4e6743
Anjo Gabriel Cordero <anjocordero@gmail.com>
2018-09-08 00:05:30 -0700

    filetype validation and row size checking

--- appender.py
+++ appender.py
@@ -2,29 +2,34 @@ import os
 import pandas as pd
 cwd = os.getcwd()
 
-def append_labels(label_name, file):
+def append_labels(labels, file):
     "Appends the column of labels in 'label_name' to the index of 'file' and transposes file"
-    labels = pd.read_excel(label_name)
+    
     data = pd.read_csv(file)
-
     index = labels.index
     data.index = index
-    data = data.T
-    data.to_csv("combined_" + file)
 
-print("Before running, make sure the current folder contains only the file containing labels, and the physiological data files corresponding to the labels. If not, press CTRL + C to abort.")
+    if data.shape[0] == labels.shape[0]:
+        data = data.T
+        data.to_csv("combined_" + file)
+
+    else:
+        print("Mismatched size in file " + file)
+
+print("Before running, make sure appender.py is in the same folder as the file containing the data and the labels to be added on to the data. If not, press CTRL + C to abort.")
 label_name = input("Enter filename of the .xlsx file which contains labels: ")
-files = os.listdir(cwd)
 
-found = files.count(label_name)     
-if found == 0:
-    print(labels + " is not found in the curent directory.")
+included_extensions = ['.csv', '.xlsx']
+files = [fn for fn in os.listdir(cwd) if any(fn.endswith(ext) for ext in included_extensions)] # Adds any files with .csv or .xlsx extensions to list
+
+if files.count(label_name) == 0: # Check if file exists
+    print(label_name + " is not found in the curent directory.")
     exit()
 
+labels = pd.read_excel(label_name)
 files.remove(label_name)
-files.remove('appender.py')
-files.remove('.git')            # Remove if not cloned from git repo!
-files.remove('README.md')       # Remove if not cloned from git repo!
 
 for file in files:
-    append_labels(label_name, file)
+    append_labels(labels, file)
+
+print("Completed!")
